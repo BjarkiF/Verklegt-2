ee0f2c326b8f4b6f4c66a21e00d13fbce534ed6d
BjarkiF <54940539+BjarkiF@users.noreply.github.com>
2020-04-29 12:24:46 +0000

    initial commit

---
+++ captain_console/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\bjark\Documents\skóli\Vor 20\Verklegt-2\captain_console\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
