2dce5504ed75fc5ab01828821b223324b6faccff
BjarkiF <54940539+BjarkiF@users.noreply.github.com>
2020-05-04 16:45:49 +0000

    item detail síða

--- captain_console/items/urls.py
+++ captain_console/items/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('all/', views.all)
+    path('all/', views.all),
+    path('<int:id>', views.get_item_by_id)
 ]
--- captain_console/items/views.py
+++ captain_console/items/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from items.models import Item
 
 def index(request):
@@ -7,3 +7,8 @@ def index(request):
 def all(request):
     context = {'items': Item.objects.all().order_by('name')}
     return render(request, 'items/all_items.html', context)
+
+def get_item_by_id(request, id):
+    return render(request, 'items/single_item_detail.html', {
+        'item': get_object_or_404(Item, pk=id)
+    })
