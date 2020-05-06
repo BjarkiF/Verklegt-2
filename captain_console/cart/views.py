26f6f3af766d74dae309114f07d0aaa2fd7837d0
BjarkiF <54940539+BjarkiF@users.noreply.github.com>
2020-05-06 11:52:54 +0000

    karfan sýnir vörur

--- captain_console/cart/views.py
+++ captain_console/cart/views.py
@@ -1,6 +1,16 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from cart.models import Cart
+from items.models import Item
+
 
 @login_required
 def index(request):
-    return render(request, 'cart/index.html')
+    cart = Cart.objects.filter(customer_id=request.user.id).first()
+    items = []
+    for cart_item in cart.items:
+        item = Item.objects.get(id=cart_item)
+        items.append(item)
+    return render(request, 'cart/index.html', {
+        'items': items
+    })
