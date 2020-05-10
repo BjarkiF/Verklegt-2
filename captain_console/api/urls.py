ac8e54611f3542ca37c66672dc9e7633b00c8e16
Jóhann Berentsson <jberentsson@gmail.com>
2020-05-10 00:35:04 +0000

    more REST routes added

--- captain_console/api/urls.py
+++ captain_console/api/urls.py
@@ -4,7 +4,10 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('all/', views.all),
+    path('items/<int:id>/', views.item.as_view({'get': 'list'})),
     path('items/all/', views.items.as_view({'get': 'list'})),
     path('cart/', views.cart.as_view({'get': 'list'})),
+    path('user/<int:id>/', views.user.as_view({'get': 'list'})),
+    path('users/', views.users.as_view({'get': 'list'})),
     path('<int:id>', views.get_item_by_id)
 ]
