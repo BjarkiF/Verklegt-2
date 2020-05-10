a942d2c2b90cc065e9c248f69fd489a28d82e2b8
Jóhann Berentsson <jberentsson@gmail.com>
2020-05-10 00:35:22 +0000

    REST tests

--- captain_console/tests/javascript/rest.test.js
+++ captain_console/tests/javascript/rest.test.js
@@ -47,22 +47,34 @@ describe('Endpoint tests', () => {
                 chai.expect(res).to.be.json;
                 chai.expect(typeof(res)).to.equal('object');
 
-                /*
-                chai.expect(res.body).to.haveOwnProperty('count')
-                chai.expect(res.body).to.haveOwnProperty('next')
-                chai.expect(res.body).to.haveOwnProperty('previous')
-                chai.expect(res.body).to.haveOwnProperty('results')
-
-                chai.expect(typeof(res.body.count)).to.equal('number')
-                chai.expect(typeof(res.body.next)).to.equal('string')
-                chai.expect(typeof(res.body.previous)).to.equal('object')
-                chai.expect(typeof(res.body.results)).to.equal('object')
-                */
-                
                 done();
         });
     });
 
+    it("GET api/v1/users/ SUCCESS - all events", function(done) {
+        chai.request('https://captain-console-47.herokuapp.com')
+            .get('/api/v1/users/')
+            .set('Content-Type', 'appliction/json')
+            .end( (err, res) => {
+                chai.expect(res).to.have.status(403);
+                chai.expect(res).to.be.json;
+                chai.expect(typeof(res)).to.equal('object');
 
+                done();
+        });
+    });
+
+    it("GET api/v1/user/1337/ SUCCESS - all events", function(done) {
+        chai.request('https://captain-console-47.herokuapp.com')
+            .get('/api/v1/user/1337/')
+            .set('Content-Type', 'appliction/json')
+            .end( (err, res) => {
+                chai.expect(res).to.have.status(403);
+                chai.expect(res).to.be.json;
+                chai.expect(typeof(res)).to.equal('object');
+
+                done();
+        });
+    });
 });
 
