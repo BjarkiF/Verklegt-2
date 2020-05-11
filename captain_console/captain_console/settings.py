0f23abbc0f288d7aebbdca76cf98410562391ff4
BjarkiF <54940539+BjarkiF@users.noreply.github.com>
2020-05-11 09:56:49 +0000

    bruh

--- captain_console/captain_console/settings.py
+++ captain_console/captain_console/settings.py
@@ -86,16 +86,15 @@ WSGI_APPLICATION = 'captain_console.wsgi.application'
 #https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 
-#DATABASES = { 'default': {
+# DATABASES = { 'default': {
 #        'ENGINE': 'django.db.backends.postgresql',
-#        'NAME': os.getenv('DB_NAME'),
-#        'USER': os.getenv('DB_USER'),
-#        'PASSWORD': os.getenv('DB_PASSWORD'),
-#        'HOST': os.getenv('DB_HOST'),
-#        'PORT': os.getenv('DB_PORT'),
-#        'OPTIONS': {'SSL_REQUIRE': False }
+#        'NAME': 'captain_console',
+#        'USER': 'postgres',
+#        'PASSWORD': 'AnnaðVerklegaNámskeiðið',
+#        'HOST': 'captainconsole47.ddns.net',
+#        'PORT': 5432,
 #    }
-#}
+# }
 
 
 
