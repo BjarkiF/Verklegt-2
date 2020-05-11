fdf12659f2e3ce3650225742eeaab9b3a1734295
BjarkiF <54940539+BjarkiF@users.noreply.github.com>
2020-05-11 09:55:15 +0000

    ???

--- captain_console/cart/models.py
+++ captain_console/cart/models.py
@@ -4,4 +4,4 @@ from django.contrib.auth.models import User
 
 class Cart(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    items = ArrayField(models.CharField(max_length=255))
+    items = ArrayField(models.CharField(max_length=255, blank=True))
---
+++ captain_console/staticfiles/js/script.js
@@ -0,0 +1,8 @@
+// src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD2m2yR2metYyz_erkHQVbs98JhZPC-fkc&callback=initMap";
+//
+// // Google maps dót
+// function initMap(){
+//     var home = {lat: 64.100730, lng: -21.898957};
+//     var map = new google.maps.Map(
+//         document.getElementById('map'), {zoom: 5, center: home});
+//     var marker = new google.maps.Marker({position: home, map: map});
