6229898568dae1a932ee93d437fc7633832d0694
BjarkiF <54940539+BjarkiF@users.noreply.github.com>
2020-05-15 17:18:08 +0000

    checkout fix

--- captain_console/cart/urls.py
+++ captain_console/cart/urls.py
@@ -6,11 +6,11 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='cart'),
-    path('add/<int:id>', views.add_to_cart, name='add_to_cart'),
-    path('remove/<int:id>', views.remove_from_cart, name='remove_from_cart'),
-    path('removeall/<int:id>', views.remove_from_cart_all, name='remove_from_cart_all'),
-    path('checkout', views.checkout),
-    path('review', views.review),
-    path('confirm', views.confirm, name='confirm_order'),
+    path('add/<int:id>/', views.add_to_cart, name='add_to_cart'),
+    path('remove/<int:id>/', views.remove_from_cart, name='remove_from_cart'),
+    path('removeall/<int:id>/', views.remove_from_cart_all, name='remove_from_cart_all'),
+    path('checkout/', views.checkout),
+    path('review/', views.review),
+    path('confirm/', views.confirm, name='confirm_order'),
 
 ]
